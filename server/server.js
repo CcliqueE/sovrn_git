1afeb2c9bc094b8c6eb428f0e759942a4259cb1a
CcliqueE <lasholdingllc@gmail.com>
2021-10-23 21:06:02 -0700

    added heroku postgres functionality!!

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 const express = require('express')
 const app = express()
 const pool = require('./db')
 const cors = require('cors')
-const { Query } = require('pg/lib')
+const bcrypt = require('bcrypt')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
@@ -11,15 +11,26 @@ app.use(express.json())
 
 //ROUTES//
 
-app.post('/', (req, res) => {
-    
-    const a = req.body.number_one
-    const b = req.body.number_two
-    
-    res.json(a * b)
+app.post('/register', async (req, res) => {
+	const query = await pool.query('SELECT email FROM users WHERE email = $1', [req.body.email])
 
+    const exist = ((query.rows).map(({ email }) => email)).toString()
+    const hashedPassword = await bcrypt.hash(req.body.password, 12)
+    const users = [req.body.email, req.body.username, hashedPassword]
+    console.log(req.body.username, hashedPassword)
+    if (exist === req.body.email) {
+        res.status(400).send(console.log('User with this email already exists'))
+    } else {
+        try {
+        const createUser = await pool.query(
+            'INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *', users)
+            res.json(createUser.rows[0])
+        } catch(err) {
+            res.status(500).send(err.message)
+        }
+    }
 })
 
 
 
-app.listen(PORT, () => {console.log('server has started on port 3000')})
+app.listen(3000, () => {console.log('server has started on port 3000')})
