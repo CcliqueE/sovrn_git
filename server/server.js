bacb7373cc5897b6ef635ed8b47ce90d3a92ce03
CcliqueE <lasholdingllc@gmail.com>
2021-11-21 23:31:30 -0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -5,9 +5,15 @@ const pool = require('./db')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 
+// const execSync = require('child_process').execSync
+
+// const output = execSync('heroku config api-inv-fou:get DATABASE_URL', { encoding: 'utf-8' })
+// console.log(output)
+
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
 
 //ROUTES//
 
@@ -17,7 +23,8 @@ app.post('/register', async (req, res) => {
     const exist = ((query.rows).map(({ email }) => email)).toString()
     const hashedPassword = await bcrypt.hash(req.body.password, 12)
     const users = [req.body.email, req.body.username, hashedPassword]
-    console.log(req.body.username, hashedPassword)
+    // console.log(req.body.username, hashedPassword)
+    // console.log(process.env.DATABASE_URL)
     if (exist === req.body.email) {
         res.status(400).send(console.log('User with this email already exists'))
     } else {
@@ -31,6 +38,30 @@ app.post('/register', async (req, res) => {
     }
 })
 
+app.post('/login', async (req, res) => {
+    const query = await pool.query('SELECT email FROM users WHERE email = $1', [req.body.email])
+    const query_two = await pool.query('SELECT password FROM users WHERE email = $1', [req.body.email])
+    const exist = ((query.rows).map(({ email }) => email)).toString()
+    const compare = ((query_two.rows).map(({ password }) => password)).toString()
 
+    const query_three = await pool.query('SELECT id FROM users WHERE email = $1', [req.body.email])
+    const boo = ((query_three.rows).map(({ id }) => id)).toString()
+
+
+    if (exist !== req.body.email) {
+        return res.status(400).send('User does not exist with this email')
+    } else {
+        try {
+            if (await bcrypt.compare(req.body.password, compare)) {
+                // console.log('user_id: ' + boo)
+                res.status(201).send(boo)
+            } else {
+                res.status(404).send("User with this password doesn't exist")
+            }
+        } catch(err) {
+            res.status(500).send(err.message)
+        }
+    }
+})
 
 app.listen(3000, () => {console.log('server has started on port 3000')})
